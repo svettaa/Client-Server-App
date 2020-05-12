cad970d74d445d90f861e7eabe49b271a3c9bea2
Sveta <svitlana.lukichova@gmail.com>
2020-05-12 19:20:53 +0300

    init

---
+++ src/main/java/com/lukichova/olenyn/app/App.java
@@ -0,0 +1,13 @@
+package com.lukichova.olenyn.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
