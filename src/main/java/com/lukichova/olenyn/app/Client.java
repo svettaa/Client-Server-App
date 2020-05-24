266b8ea8917e5b7b913d264e9c429066872d435b
Sveta <svitlana.lukichova@gmail.com>
2020-05-24 13:00:24 +0300

    Client new methods

--- src/main/java/com/lukichova/olenyn/app/Client.java
+++ src/main/java/com/lukichova/olenyn/app/Client.java
@@ -10,24 +10,44 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
+    private TCPNetwork network;
+
+    Client(){}
 
     public static void main(String[] args) {
         Message testMessage = new Message(1, 1, "time");
         Packet packet = new Packet((byte) 1, UnsignedLong.ONE, testMessage);
 
+/*
         Message secondTestMessage = new Message(1, 1, "notTime");
         Packet secondPacket = new Packet((byte) 1, UnsignedLong.ONE, secondTestMessage);
+*/
 
         try {
-            Socket socket = new Socket("localhost",2305);
-            Network network = new TCPNetwork(socket);
+            Client client = new Client();
+            client.connect(2305);
 
-            network.send(packet);
-            network.receive();
+            client.request(packet);
 
-            network.close();
+            client.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void connect(int serverPort) throws Exception {
+        network = new TCPNetwork(new Socket("localhost", serverPort));
+    }
+
+    public Packet request(Packet packet) throws Exception {
+        if (network == null) {
+            throw new IllegalStateException("Not connected yet");
+        }
+        network.send(packet);
+        return network.receive();
+    }
+
+    public void disconnect() throws Exception {
+        network.close();
     }
 }
