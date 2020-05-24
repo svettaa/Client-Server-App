c5ae04d35cd86aa93332a5b0877267de22b92347
SofiyaOlenyn <olenynsofiya@gmail.com>
2020-05-24 16:24:14 +0300

    add ProcessorTests

---
+++ src/test/java/com/lukichova/olenyn/app/ProcessorTest.java
@@ -0,0 +1,35 @@
+package com.lukichova.olenyn.app;
+
+import com.google.common.primitives.UnsignedLong;
+import com.lukichova.olenyn.app.classes.Processor;
+import com.lukichova.olenyn.app.entities.Message;
+import com.lukichova.olenyn.app.entities.Packet;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProcessorTest {
+    private Message testMessage;
+    private Packet packet;
+    UnsignedLong unsignedLongbPktId = UnsignedLong.valueOf(Long.MAX_VALUE);
+
+    @Test
+    public void process1() {
+
+        unsignedLongbPktId = unsignedLongbPktId.plus(UnsignedLong.valueOf("2305"));
+        testMessage = new Message(3, 4, "time");
+        packet = new Packet((byte) 1, unsignedLongbPktId, testMessage);
+        Packet answer = Processor.process(packet);
+        Assert.assertEquals("now()",answer.getBMsq().getMessage());
+    }
+    @Test
+    public void process2() {
+
+        unsignedLongbPktId = unsignedLongbPktId.plus(UnsignedLong.valueOf("2305"));
+        testMessage = new Message(3, 4, "notime");
+        packet = new Packet((byte) 1, unsignedLongbPktId, testMessage);
+        Packet answer = Processor.process(packet);
+        Assert.assertEquals("other",answer.getBMsq().getMessage());
+    }
+}
