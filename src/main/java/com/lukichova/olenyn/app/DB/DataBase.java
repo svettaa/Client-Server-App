a7001b1596eb6d3ebaaeeb5750f40d6b888d43f4
SofiyaOlenyn <olenynsofiya@gmail.com>
2020-06-04 15:04:36 +0300

    add GoodsDao

--- src/main/java/com/lukichova/olenyn/app/DB/DataBase.java
+++ src/main/java/com/lukichova/olenyn/app/DB/DataBase.java
@@ -1,4 +1,35 @@
 package com.lukichova.olenyn.app.DB;
 
+import java.sql.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.lukichova.olenyn.app.resoures.Resoures.DATABASE_NAME;
+
 public class DataBase {
+    static Connection connection;
+
+    public static void connect() {
+        try {
+            String url = "jdbc:sqlite:" + DATABASE_NAME;
+            connection = DriverManager.getConnection(url);
+
+            System.out.println("Connection to SQLite has been established.");
+            System.out.println();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void close() {
+        try {
+            connection.close();
+
+            System.out.println("Connection closed");
+            System.out.println();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
--- src/main/java/com/lukichova/olenyn/app/resoures/Resoures.java
+++ src/main/java/com/lukichova/olenyn/app/resoures/Resoures.java
@@ -1,7 +1,8 @@
 package com.lukichova.olenyn.app.resoures;
 
 public class Resoures {
-
+    public static final String DATABASE_NAME = "test.db";
+    public final static String GOODS_TABLE = "goods";
     public static final String NETWORK_TYPE = "udp";
     public static final String NETWORK_HOST = "localhost";
     public static final int NETWORK_PORT = 2035;
